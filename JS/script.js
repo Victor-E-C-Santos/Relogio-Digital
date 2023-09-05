55523b7edc607cbbcd2a52a6dab9222efd107bc5
Victor Edilmo Cardoso santos <118738444+Victor-E-C-Santos@users.noreply.github.com>
2023-09-05 17:59:24 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time(){
+    let dataToday = new Date();
+    let hr = dataToday.getHours();
+    let min = dataToday.getMinutes();
+    let sec = dataToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (hr < 10) min = '0' + min;
+
+    if (hr < 10) sec = '0' + sec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec; 
+
+});
